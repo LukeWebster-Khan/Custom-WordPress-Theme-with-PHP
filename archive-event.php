1fc23d567e7aa5532fb8827e9a3703b26c772422
LukeWebster-Khan <Luke.Webster-Khan@theaccesspoint.co.uk>
2021-11-10 18:29:59 +0000

    added month variable and event recap href

--- archive-event.php
+++ archive-event.php
@@ -18,8 +18,11 @@ get_header();
     the_post();?>
     <div class="event-summary">
         <a class="event-summary__date t-center" href="#">
-            <span class="event-summary__month">Mar</span>
-            <span class="event-summary__day">25</span>
+            <span class="event-summary__month"><?php 
+                    $eventDate = new DateTime(get_field('event_date'));
+                    echo $eventDate->format('M');
+                    ?></span>
+            <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>
         </a>
         <div class="event-summary__content">
             <h5 class="event-summary__title headline headline--tiny"><a
@@ -31,6 +34,9 @@ get_header();
     <?php }
     echo paginate_links();
     ?>
+    <hr class="section-break">
+    <p>Looking for a recap of past Events?<a href="<?php echo site_url('/past-events') ?>">Check our our past events
+            archive!</a></p>
 
 </div>
 
