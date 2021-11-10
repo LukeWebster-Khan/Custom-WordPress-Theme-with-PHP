7014ea4bb7362e4c8d35143d4b31e4d5b1332f97
LukeWebster-Khan <Luke.Webster-Khan@theaccesspoint.co.uk>
2021-11-10 18:38:31 +0000

    test

--- front-page.php
+++ front-page.php
@@ -9,7 +9,8 @@
         <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
         <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re
             interested in?</h3>
-        <a href="#" class="btn btn--large btn--blue">Find Your Major</a>
+        <a href="<?php echo get_post_type_archive_link('program'); ?>" class="btn btn--large btn--blue">Find Your
+            Major</a>
     </div>
 </div>
 
@@ -18,17 +19,33 @@
         <div class="full-width-split__inner">
             <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
 
-            <?php $homePageEvents = new WP_Query(array(
-                'posts_per_page'=>2,
-                'post_type'=> 'event'
+            <?php   
+            $today = date('Ymd');
+            $homePageEvents = new WP_Query(array(
+                'posts_per_page'=> 2,
+                'post_type'=> 'event',
+                'meta_key'=> 'event_date',
+                'orderby'=> 'meta_value_num',
+                'order'=> 'ASC',
+                'meta_query' => array(
+                    array(
+                        'key' => 'event_date',
+                        'compare'=> '>=',
+                        'value' => $today,
+                        'type'=> 'numeric'
+                    )
+                )
             ));
 
             while($homePageEvents->have_posts()){
                 $homePageEvents->the_post();?>
             <div class="event-summary">
                 <a class="event-summary__date t-center" href="#">
-                    <span class="event-summary__month">Mar</span>
-                    <span class="event-summary__day">25</span>
+                    <span class="event-summary__month"><?php 
+                    $eventDate = new DateTime(get_field('event_date'));
+                    echo $eventDate->format('M');
+                    ?></span>
+                    <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>
                 </a>
                 <div class="event-summary__content">
                     <h5 class="event-summary__title headline headline--tiny"><a
