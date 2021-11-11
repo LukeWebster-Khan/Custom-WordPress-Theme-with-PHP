b9c3ca9301036b2d23c4abb5b1eaddc56dd9400e
LukeWebster-Khan <Luke.Webster-Khan@theaccesspoint.co.uk>
2021-11-11 21:23:27 +0000

    added dynamic banner content and thumbnails

--- single-professor.php
+++ single-professor.php
@@ -3,25 +3,36 @@
     while(have_posts()) {
         the_post(); ?>
 <div class="page-banner">
-    <div class="page-banner__bg-image"
-        style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>)"></div>
+    <div class="page-banner__bg-image" style="background-image: url(<?php
+         $pageBannerImage = get_field('page_banner_background_image'); echo
+          $pageBannerImage['sizes']['pageBanner'] ?>)">
+    </div>
     <div class="page-banner__content container container--narrow">
         <h1 class="page-banner__title"><?php the_title(); ?></h1>
         <div class="page-banner__intro">
-            <p>DON'T FORGET TO REPLACE ME LATER</p>
+            <p><?php the_field('page_banner_subtitle') ?></p>
         </div>
     </div>
 </div>
 <div class="container container--narrow page-section">
 
 
-    <div class="generic-content"><?php the_content(); ?></div>
+    <div class="generic-content">
+        <div class="row group">
+            <div class="one-third">
+                <?php the_post_thumbnail('professorPortrait'); ?>
+            </div>
+            <div class="two-thirds">
+                <?php the_content(); ?>
+            </div>
+        </div>
+    </div>
 
     <?php
      $relatedPrograms = get_field('related_programs'); 
      if($relatedPrograms){
         echo '<hr class="section-break"></hr>';
-        echo '<h2 class="headline headline--medium">Related Program(s)</h2>';
+        echo '<h2 class="headline headline--medium">Subject(s) Taught</h2>';
         echo '<ul class="link-list min-list">';
         foreach($relatedPrograms as $program){  ?>
     <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo
