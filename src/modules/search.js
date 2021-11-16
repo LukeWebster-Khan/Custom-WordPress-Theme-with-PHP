1eb71e633177bcbce35a23289753225b84a737d8
LukeWebster-Khan <Luke.Webster-Khan@theaccesspoint.co.uk>
2021-11-16 11:26:15 +0000

    test

--- src/modules/search.js
+++ src/modules/search.js
@@ -1,26 +1,39 @@
 import $ from "jquery";
 
 class Search {
-  // 1. describes our object
+  // 1. describe and create/initiate our object
   constructor() {
     this.openButton = $(".js-search-trigger");
     this.closeButton = $(".search-overlay__close");
     this.searchOverlay = $(".search-overlay");
+    this.searchField = $("#search-term");
     this.events();
     this.isOverlayOpen = false;
+    this.typingTimer;
   }
-  // 2. Events
+
+  // 2. events
   events() {
     this.openButton.on("click", this.openOverlay.bind(this));
     this.closeButton.on("click", this.closeOverlay.bind(this));
     $(document).on("keydown", this.keyPressDispatcher.bind(this));
+    this.searchField.on("keydown", this.typingLogic.bind(this));
   }
+
   // 3. methods (function, action...)
+  typingLogic() {
+    clearTimeout(this.typingTimer);
+    this.typingTimer = setTimeout(function () {
+      console.log("This is a timeout test.");
+    }, 2000);
+  }
 
   keyPressDispatcher(e) {
     if (e.keyCode == 83 && !this.isOverlayOpen) {
       this.openOverlay();
-    } else if (e.keyCode == 27 && this.isOverlayOpen) {
+    }
+
+    if (e.keyCode == 27 && this.isOverlayOpen) {
       this.closeOverlay();
     }
   }
@@ -28,13 +41,14 @@ class Search {
   openOverlay() {
     this.searchOverlay.addClass("search-overlay--active");
     $("body").addClass("body-no-scroll");
-    console.log("our open method just run");
+    console.log("our open method just ran!");
     this.isOverlayOpen = true;
   }
+
   closeOverlay() {
     this.searchOverlay.removeClass("search-overlay--active");
     $("body").removeClass("body-no-scroll");
-    console.log("our close method just run");
+    console.log("our close method just ran!");
     this.isOverlayOpen = false;
   }
 }
